c5e3eb11b664ba97ab851e5922493be6a3c74e52
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-03-03 19:11:08 +0100

    preview mode api configured

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { previewData } from 'next/headers';
 
 const HomePage = () => {
-	return <div className='text-5xl'>HomePage</div>;
+	if (previewData()) {
+		return <>I am in preview mode</>;
+	}
+	return <div className='text-5xl'>HomePage, not in preview mode</div>;
 };
 
 export default HomePage;
