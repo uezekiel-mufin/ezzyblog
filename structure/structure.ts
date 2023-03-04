6cd8fd2a615dbd3311679b553cd60792bbedcb0c
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-03-04 17:48:15 +0100

    add live preview on sanity studio

---
+++ structure/structure.ts
@@ -0,0 +1,31 @@
+import { DefaultDocumentNodeResolver } from 'sanity/desk';
+import Iframe from 'sanity-plugin-iframe-pane';
+
+// Import this into the deskTool() plugin
+export const defaultDocumentNode: DefaultDocumentNodeResolver = (
+	S,
+	{ schemaType }
+) => {
+	// Only show preview pane on `movie` schema type documents
+	switch (schemaType) {
+		case `post`:
+			return S.document().views([
+				S.view.form(),
+				S.view
+					.component(Iframe)
+					.options({
+						url: `${
+							process.env.NEXT_VERCEL_URL || 'http://localhost:3000/'
+						}/api/preview`,
+						defaultSize: 'desktop',
+						reload: {
+							button: true,
+						},
+						attributes: {},
+					})
+					.title('Preview'),
+			]);
+		default:
+			return S.document().views([S.view.form()]);
+	}
+};
