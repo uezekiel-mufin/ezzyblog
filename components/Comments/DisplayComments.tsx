4fbd3ff6d0082c9948be53cb27ba6a5ad9e535e8
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-03-18 22:08:00 +0100

    add conditional rendering of the reply form

--- components/Comments/DisplayComments.tsx
+++ components/Comments/DisplayComments.tsx
@@ -10,19 +10,24 @@ type Props = {
 };
 const DisplayComments = ({ comment, firstParentId }: Props) => {
 	const [showReplyBox, setShowReplyBox] = useState(false);
-	const toggleReplyBox = () => setShowReplyBox(!showReplyBox);
+	const [toggleId, setToggleId] = useState('');
+	const toggleReplyBox = (id: string) => {
+		setToggleId(id);
+		setShowReplyBox(!showReplyBox);
+	};
 
 	return (
 		<main className='my-12 bg-gray-100 rounded-lg'>
-			<section className='border-pink-200 space-y-4 border px-4 py-8'>
+			<section className='border-pink-200 space-y-4 border px-4 pb-8 pt-4'>
+				<h2 className='text-2xl font-bold'>Comments</h2>
 				{comment.map((comment) => (
 					<div key={comment._id} className='p-5 border-gray-400 border'>
 						<SingleComment firstParentId={firstParentId} comment={comment} />
 
-						<button onClick={toggleReplyBox} className='font-semibold'>
-							{showReplyBox ? 'Cancel' : 'Reply'}
+						<button onClick={() => toggleReplyBox(comment._id)} className=''>
+							{showReplyBox && comment._id === toggleId ? 'Cancel' : 'Reply'}
 						</button>
-						{showReplyBox && (
+						{showReplyBox && comment._id === toggleId && (
 							<AddReply
 								parentCommentId={comment._id}
 								firstParentId={firstParentId || comment._id}
